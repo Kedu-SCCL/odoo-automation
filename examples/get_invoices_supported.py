96d7b3f2eaf5544970847fb6f2066f1e38b2478a
Javier Legido <javier.legido@kedu.coop>
2019-11-22 14:20:58 +0100

    Added example to get supported invoices

---
+++ examples/get_invoices_supported.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+from odoo_automation import Odoo
+
+from settings import odoo_url, odoo_db, odoo_user, odoo_password
+
+o = Odoo(url = odoo_url, db = odoo_db, user = odoo_user, password =
+         odoo_password)
+
+fields = ('ref', 'date', 'invoice_partner_display_name',
+          'amount_untaxed', 'amount_tax', 'amount_total',
+          'invoice_payment_state')
+print(o.get_invoices('in_invoice', fields))
